4a34c7b6eaf99d85bb09f00b2e43561495a7975c
Jacob M Miller <jmiller99@uiowa.edu>
2024-08-21 13:04:54 -0500

    Created FFtoXML groovy script and test.

--- modules/potential/src/main/java/ffx/potential/parsers/OpenMMXmlFilter.java
+++ modules/potential/src/main/java/ffx/potential/parsers/OpenMMXmlFilter.java
@@ -48,6 +48,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.logging.Logger;
@@ -66,6 +67,9 @@ public class OpenMMXmlFilter {
     /** The ForceField object that will be written to an XML. */
     private final ForceField forceField;
 
+    /** The path to write the XML file to. Should not include '.xml' on end. */
+    private String outputName;
+
     /**
      * Constructor for outputting XML.
      *
@@ -73,6 +77,17 @@ public class OpenMMXmlFilter {
      */
     public OpenMMXmlFilter(ForceField forceField) {
         this.forceField = forceField;
+    }
+
+    /**
+     * Constructor for outputting XML with output path specified.
+     *
+     * @param forceField a {@link ffx.potential.parameters.ForceField} object.
+     * @param saveName a {@link java.io.File} object.
+     */
+    public OpenMMXmlFilter(ForceField forceField, String saveName) {
+        this.forceField = forceField;
+        this.outputName = saveName;
     }
 
     /**
@@ -356,7 +371,7 @@ public class OpenMMXmlFilter {
         }
 
         // Write XML to 'force field name'.xml
-        writeXML(doc, forceField.getString("forcefield", "UNKNOWN"));
+        writeXML(doc);
     }
 
     /**
@@ -427,10 +442,16 @@ public class OpenMMXmlFilter {
     /**
      * Create an OpenMM-style XML file from the Document object that was created in toXML().
      * @param doc Document object containing XML nodes.
-     * @param outputName String with name of the new file.
      * @throws TransformerException
      */
-    private static void writeXML(Document doc, String outputName) throws TransformerException {
+    private void writeXML(Document doc) throws TransformerException {
+        String saveName;
+        if (outputName != null) {
+            saveName = outputName;
+        } else {
+            saveName = forceField.getString("forcefield", "UNKNOWN");
+        }
+
         TransformerFactory tfFactory = TransformerFactory.newInstance();
         Transformer transformer = tfFactory.newTransformer();
 
@@ -438,7 +459,7 @@ public class OpenMMXmlFilter {
         transformer.setOutputProperty(OutputKeys.INDENT, "yes"); // print with indentation
 
         DOMSource src = new DOMSource(doc);
-        StreamResult result = new StreamResult(outputName + ".xml");
+        StreamResult result = new StreamResult(saveName + ".xml");
         transformer.transform(src,result);
     }
 }
